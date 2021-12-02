50ee1c52fcf7d23cede6a05ae227601d2f5eade6
David Nash <hisownspace@gmail.com>
2021-12-02 16:31:04 -0500

    finished phase 4

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,4 +1,5 @@
 const ADD_TO_CART = 'cart/addToCart';
+const REMOVE_FROM_CART = 'cart/removeFromCart';
 
 export const addToCart = (id) => {
     return {
@@ -7,27 +8,26 @@ export const addToCart = (id) => {
     }
 }
 
-const initialState = {
-    cart: {},
-    produce: {}
+export const removeFromCart = (id) => {
+    return {
+        type: REMOVE_FROM_CART,
+        id
+    }
 }
 
-export const cartReducer = (state = initialState, action) => {
+export const cartReducer = (state = {}, action) => {
     const currentId = action.id;
-
+    let newState = {...state};
     switch (action.type) {
         case ADD_TO_CART:
-            const newState = {
-                ...state,
-                cart: {
-                    ...state.cart,
-                    currentId: {
-                        id: action.id,
-                        count: 1
-                    }
-                }
-            };
-
+            newState = {...state};
+            newState[currentId] = {id: action.id, count: 1}
             return newState;
-    }
+        case REMOVE_FROM_CART:
+            newState = {...state};
+            delete newState[currentId];
+            return newState;
+        default:
+            return state;
+    };
 }
